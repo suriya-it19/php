46ee95ed614a451a6bd9508d71669825d566a8eb
suriya-it19 <suriya.it19@bitsathy.ac.in>
2019-08-21 13:44:59 +0530

    firstcommit

---
+++ display.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>SAMPLE</title>
+</head>
+<body>
+<?php
+echo"<center><b>My Name Is</b><br /><b>DHANUSH</b>";
+$a="NEYMARJR.";
+echo"<center>$a</center>";
+$a="1";
+$b="6";
+echo"$a+$b";
+?>
+</body>
+</html>
---
+++ print.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<?php
+for($a=1;$a<=10;$a++)
+echo"number is $a<br>";
+?>
+</body>
+</html>
